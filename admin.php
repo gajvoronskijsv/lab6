6f4004ec3ea01102bc4ee8bb6ce72c833cfb732a
gajvoronskijsv <stas_gajvoronskij@mail.ru>
2020-05-07 17:54:22 +0300

    first commit

--- admin.php
+++ admin.php
@@ -19,7 +19,8 @@ if(isset($_POST['OldAuth'])){
 else{
   $p=0;
 }
-if (empty($_SERVER['PHP_AUTH_USER'])) {
+if (!isset($_SERVER['PHP_AUTH_USER']) ||
+isset($_POST['SeenBefore']) && $p == $_SERVER['PHP_AUTH_USER']) {
   authenticate(); 
 }
 else{
@@ -44,7 +45,7 @@ else{
     echo "<input type='submit' name='sendform' id='out' value='Создать нового пользователя'/>\n";
     echo "<input type='submit' name='sendform' id='out' value='Войти как пользователь'/>\n";
     echo "</form></p>\n";
-    $_SERVER['PHP_AUTH_USER']=0;
+    //$_SERVER['PHP_AUTH_USER']=0;
     //$_SERVER['PHP_AUTH_PW']=0;
     $num=1;
     $messages[] = sprintf("
@@ -130,7 +131,7 @@ else{
     }
   }
   else{
-  	$_SERVER['PHP_AUTH_USER']=0;
+  	//$_SERVER['PHP_AUTH_USER']=0;
     //$_SERVER['PHP_AUTH_PW']=0;
     authenticate();
   }
